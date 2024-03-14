d6c650a50f0cb4be550c989a7b23254fbaa5039c
Courtney Smith <Court@Sebs-Mac-mini.local>
2024-03-14 19:00:09 +0000

    Created a pseudo code plan of what I'm exactly testing

---
+++ main.test.js
@@ -0,0 +1,29 @@
+// import { defineConfig } from 'vitest/config'
+// import the vitest configuration
+
+
+// Test to check whether the return is a boolean
+
+// Test to check whether the value is correct - if it's within 180 mins it should return true
+// Example input 2, 100 - expected output false
+
+// Test to check whether the value is correct - if it's outside 180 mins it should return false
+// Example input 1, 100 - expected output true
+
+// Test to check whether the value is correct - if it's within 180 mins it should return true
+// Example input 2, 50 - expected output true
+
+// Test to check whether the value is correct - if it's outside 180 mins it should return false
+// Example input 7, 50 - expected output false
+
+// Test to check whether the value is correct - if it's within 180 mins it should return true
+// Example input 0.5, 100 - expected output true
+
+// Test to check whether the value is correct - if it's outside 180 mins it should return false
+// Example input 5, 200 - expected output true
+
+// Test if unwanted characters are being called
+
+// Test if nothing is called - if empty then it's failed
+
+// Test if integers are positive
