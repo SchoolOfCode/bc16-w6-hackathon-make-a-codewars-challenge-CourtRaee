a968cd599b69c974fd14782bbb47944261e122ee
Courtney Smith <Court@Sebs-Mac-mini.local>
2024-03-15 09:29:26 +0000

    Completed test 5 - check if return is false

--- main.test.js
+++ main.test.js
@@ -34,6 +34,11 @@ test("should check is the value is correct - return true", () => {
 
 // Test to check whether the value is correct - if it's outside 180 mins it should return false
 // Example input 7, 50 - expected output false
+test("should check is the value is correct - return false", () => {
+  const expected = false;
+  const actual = legoBuild(7, 50);
+  expect(actual).toBe(expected);
+});
 
 // Test to check whether the value is correct - if it's within 180 mins it should return true
 // Example input 0.5, 100 - expected output true
